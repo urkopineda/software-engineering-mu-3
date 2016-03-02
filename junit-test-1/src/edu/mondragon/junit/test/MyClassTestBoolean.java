03e072356db0de9aea6a26c0f62ec61fb51387da
Urko Pineda <urkopineda@gmail.com>
2016-03-02 11:49:23 +0100

    Added JUnit Test 1

---
+++ junit-test-1/src/edu/mondragon/junit/test/MyClassTestBoolean.java
@@ -0,0 +1,23 @@
+package edu.mondragon.junit.test;
+
+import static org.junit.Assert.*;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import edu.mondragon.junit.first.MyClass;
+
+import org.junit.*;
+
+public class MyClassTestBoolean {
+	
+	@Test
+	public void testBoolean() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+		MyClass p = new MyClass();
+		Method m = MyClass.class.getDeclaredMethod("methodToBeTested", String.class);
+		m.setAccessible(true);
+		boolean result = (boolean) m.invoke(new MyClass(), "hola");
+		assertEquals("10 x 5 es 50", false, result);
+	}
+
+}
